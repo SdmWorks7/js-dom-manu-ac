7bb90c99fee87571bf97ee573e2df35ed793400e
Saumyadeep Mishra <sdm.cord7@gmail.com>
2026-07-15 16:11:01 +0530

    prac queses, dom manu

---
+++ app.js
@@ -0,0 +1,22 @@
+let para = document.createElement("p");
+para.innerText="Hey I'm red";
+document.querySelector('body').append(para);
+para.classList.add("red");
+
+let head = document.createElement("h3");
+head.innerText="I'm a blue h3";
+document.querySelector("body").append(head);
+head.classList.add("blue");
+
+ let div= document.createElement("div");
+ div.classList.add("design");
+ document.querySelector("body").append(div);
+ 
+ let ch1= document.createElement("h1");
+ ch1.innerText="I'm in a div";
+ let cp= document.createElement("p");
+ cp.innerText="ME TOO!";
+
+ div.appendChild(ch1);
+ div.appendChild(cp);
+
